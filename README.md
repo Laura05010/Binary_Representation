9bbff9433ea3b4fdee879d384c5765cbf5b0ce21
Laura05010 <lcmmadrid5@gmail.com>
2020-07-31 02:52:15 -0400

    created binary conversion and sum representation functions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Binary_Representation
+# Binary representation
+
+## This program was made to interpret a number as a sum of powers of two.
+## Inspired by the Theorem presented on the introduction to mathematical proofs MAT102.
+## Theorem: Every natural number n >= 1 can be represented as a sum of distinct non-negative integer powers of 2.
---
+++ program.py
@@ -0,0 +1,41 @@
+import itertools
+import sys
+
+
+def main_menu():
+    print("^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-")
+    print("^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-")
+    print("^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-")
+    print("PYTHON BINARY REPRESENTATION")
+    print("^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-")
+    n = int(input("Please enter a natural number to represent it as a sum of distinct non-negative integer powers of 2:"))
+    print(convert_to_binary(n))
+    print(sum_representation(convert_to_binary(n)))
+
+def convert_to_binary(n:int) -> str:
+    rep = ""
+    while n > 0:
+        if n%2 == 1:
+            rep =  str("1") + rep
+            n -= 1
+        
+        elif n%2 == 0:
+            rep =  str("0") + rep
+        
+        n = n//2 
+    return rep
+
+def sum_representation(rep:str) -> str:
+    sum_rep = ""
+    rev_rep = rep[::-1]
+    for i in range(len(rev_rep)):
+        if rev_rep[i] == "1":
+            sum_rep += f"+ 2^{i} " #f is formatting the string
+    
+    return f"{rep} = {sum_rep[2:]}"
+
+if __name__ == '__main__':
+    main_menu()
+    
+
+
